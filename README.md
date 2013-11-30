858c93c2598ba0e609741549f199f4e4ef1fde8a
radiocontrolled <radiocontrolled@gmail.com>
2013-11-30 13:13:56 +0000

    joining refugee data with map data, and adding tooltips

--- README.md
+++ README.md
@@ -2,5 +2,5 @@ I tested this in Chrome, Firefox and Safari in a MAMP local hosting environment 
 Cross origin requests (e.g. used in d3.json) require HTTP - you'll get a XMLHttpRequest error in Chrome 
 and Safari if you don't load over HTTP. 
 
-This map adapts Visible Data's <a href="http://eyeseast.github.io/visible-data/2013/08/26/responsive-d3/">Responsive Map</a> 
-code. I've used jQuery to detect the window size. 
+Visible Data's <a href="http://eyeseast.github.io/visible-data/2013/08/26/responsive-d3/">Responsive Map</a> was the initial inspiration
+but I changes, like jQuery to detect the window size, simplifying resize(), and forgoing Bootstrap by doing my own media queries and tooltip.   
--- map.js
+++ map.js
@@ -12,21 +12,74 @@ var projection = d3.geo.mercator();
 
 //Define path generator
 var path = d3.geo.path().projection(projection);
-			
+d3.csv("migrants_by_region.csv", function(data) {
  d3.json("canada.json", function(json) {
- 	
+
  	var svg = d3.select("article")
 		.append("svg")
 		.attr("width",width)
 		.attr("height",height);
+				
+							
+				
+	    //loop through the CSV
+		for (var i = 0; i < data.length; i ++){
+						
+		//get the province/region name
+		var provinceName = data[i].iso_3166_2;
+						
+		//get the number of migrants in that province/region
+		var numberOfMigrants = data[i].migrants;
+						
+		for(var j = 0; j < json.features.length; j++){
+							
+			if (json.features[j].properties.iso_3166_2 == provinceName){
+								
+				//make an entry into the JSON with the value of the # of migrants
+				json.features[j].properties.value = numberOfMigrants;
+
+				//stop
+				break;
+			}	
+						
 	//draw map
 	var map = svg.selectAll("path")
 		.data(json.features)
 		.enter()
 		.append("path")
 		.attr("d", path)
-		.style("fill", "#3498db"); 
+		.style("fill", "#3498db")
+			.on("mouseover",function(d,i){	
+					//grab the coordinates of the mouseover				
+					var coordinates = d3.mouse(this);
+
+					d3.select(this).classed("highlight",true)
+					d3.select("#tooltip")
+					.style({
+							"left": coordinates[0] + "px",
+							"top": coordinates[1] + "px"
+						})
+						.classed("hidden",false)
+						.select("#migrants")
+						
+						.append("text")
+						.classed("hidden",false)
+						.text(function(){
+							return d.properties.name + ", " + d.properties.value;
+						})
+				    })
+			.on("mouseout", function(d,i){
+					
+				    d3.select(this).classed("highlight",false)	
+				    d3.select("#tooltip")
+				         .classed("hidden",true)	
+				    d3.select("#migrants text").remove();
+				})
+				         
+			}			
+		 }		
  	})
+});
 
 
 d3.select(window).on('resize', resize);
